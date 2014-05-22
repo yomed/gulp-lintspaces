f018708a3a3f6c0e19577bf64376d320bc7272b4
Christopher Knötschke <cknoetschke@gmail.com>
2014-05-22 14:12:28 +0200

    added reporter
    fixes #2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var
 	es 			= require('event-stream'),
-	Lintspaces 	= require('lintspaces')
+	Lintspaces 	= require('lintspaces'),
+	colors      = require('gulp-util').colors
 ;
 
 module.exports = function(options) {
@@ -17,3 +18,27 @@ module.exports = function(options) {
 		return this.emit('data', file);
 	});
 };
+
+module.exports.reporter = function() {
+	return es.through(function(file) {
+		if (file.isNull()) {
+			return this.emit('data', file);
+		}
+
+		if(file.lintspaces && Object.keys(file.lintspaces).length) {
+			for(var line in file.lintspaces) {
+				file.lintspaces[line].forEach(function(error) {
+					console.error(
+						'[%s] %s in (%s:%d)\n',
+						colors.green('gulp-lintspaces'),
+						colors.red(error),
+						file.path,
+						line
+					);
+				});
+			}
+		}
+
+		return this.emit('data', file);
+	});
+}
